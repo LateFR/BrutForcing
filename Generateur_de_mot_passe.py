ebc5bffa1661e6c2cd1bc2c69114ae7be0b8fb60
Phoenix-dev-RLis <rigo.lucas@icloud.com>
2024-11-10 03:45:45 +0100

    Update Generateur_de_mot_passe.py

--- Generateur_de_mot_passe.py
+++ Generateur_de_mot_passe.py
@@ -1,19 +1,6 @@
 import string
 import random
 
-longueur=int(input("Quel longueur pour le mot de passe ? Répondre par un nombre entier: "))
-
-try:
-    inclure_chiffre=bool(input("Mettre des chiffre ? Répondre par True ou False: "))
-except ValueError:
-    print("Erreur: La réponse doit être True ou False")
-
-try:
-    inclure_caractere_speciaux=bool(input("Mettre des cacrctère spéciaux ? Répondre par True ou False: "))
-except ValueError:
-    print("Erreur: La réponse doit être True ou False")
-
-longueur=int(input("Quel longueur pour le mot de passe ? Répondre par un nombre entier: "))
 
 def generer_mot_de_passe(longueur,inclure_chiffre,inclure_caractere_speciaux):
     caractere_du_mot_de_passe=string.ascii_letters
@@ -40,8 +27,6 @@ if __name__=="__main__":
         inclure_caractere_speciaux=bool(input("Mettre des cacrctère spéciaux ? Répondre par True ou False: "))
     except ValueError:
         print("Erreur: La réponse doit être True ou False")
-
-    longueur=int(input("Quel longueur pour le mot de passe ? Répondre par un nombre entier: "))
 
     print(generer_mot_de_passe(longueur,inclure_chiffre,inclure_caractere_speciaux))
     print(random.choice(string.ascii_letters))
